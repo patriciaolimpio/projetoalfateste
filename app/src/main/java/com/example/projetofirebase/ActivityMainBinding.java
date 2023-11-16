cd394647cd84a9beb6837d7d1b669c56c778c6e5
Patricia Olimpio <93798100+patriciaolimpio@users.noreply.github.com>
2023-11-16 09:58:04 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projetofirebase/ActivityMainBinding.java
@@ -0,0 +1,20 @@
+package com.example.projetofirebase;
+
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.appcompat.widget.Toolbar;
+
+public class ActivityMainBinding {
+    public Toolbar toolbar;
+    public View fab;
+
+    public static ActivityMainBinding inflate(LayoutInflater layoutInflater) {
+
+        return null;
+    }
+
+    public int getRoot() {
+        return 0;
+    }
+}
